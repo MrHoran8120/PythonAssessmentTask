6d4748e9f89c063b39629b342586f3b8d2292b6e
MrHoran8120 <fake@fakex.com>
2021-10-22 14:15:40 +1100

    oops uploaded wrong file

--- 05forLoop.py
+++
@@ -1,9 +0,0 @@
-print(" ------------------------------------------------")
-print("|                                                |")
-print("|    05forLoop                                   |")
-print("|    Name : Mr Horan                             |")
-print("|    Version : 01                                |")
-print("|                                                |")
-print(" ------------------------------------------------")
-for x in range(1,500):
-        print (x)
