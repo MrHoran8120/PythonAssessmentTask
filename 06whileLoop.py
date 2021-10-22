5e83b7735750ec9a3a5962ea36619f9b6e7dd510
MrHoran8120 <fake@fakex.com>
2021-10-22 14:07:55 +1100

    Update for bugs

---
+++ 06whileLoop.py
@@ -0,0 +1,14 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    06whileLoop                                 |")
+print("|    Name : Mr Horan                             |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+x=""
+while x != "IST":
+    x = input ("What is the name of this subject ")
+    if (x!="IST"):
+        print("Not Correct - try again")
+    else:
+        print("\n\n Congratulations!!\n\n")
